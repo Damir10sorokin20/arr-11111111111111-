7aa9f6e5969080a8d3a0393f5dad233d83ce059b
Damir10sorokin20 <114170327+Damir10sorokin20@users.noreply.github.com>
2022-11-06 19:10:19 +0300

    Add files via upload

---
+++ arr[1111111111].cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include<ctime>
+using namespace std;
+int main() {
+ 
+	//const int n = 10;
+	//int mas[n];
+	//srand(time(nullptr));
+	//for (int i = 0; i < n; i++) {
+		//mas[i] = rand()%51-25;
+		//cout << mas[i] << ' ';
+	//}
+	//int k = 0;
+	//for (int i = 0; i < n; i++) {
+		//if (mas[i] > 0) k++;
+	//}
+	//cout << k;
+
+	//cам 1
+	//const int n = 10;
+	//int s = 0;
+	//int mas[n];
+	//srand(time(nullptr));
+	//for (int i = 0; i < n; i++) {
+		//mas[i] = rand() % 31 - 10;
+		//cout << mas[i] << ' ';
+	//}
+    
+	//for (int i = 0; i < n; i++) {
+		//if (mas[i] % 2 == 0);
+		//s = s + mas[i];
+	//}
+	//cout << s;
+
+	//сам 2 
+	//const int n = 10;
+	//int k = 0;
+	//int mas[n];
+	//srand(time(nullptr));
+	//for (int i = 0; i < n; i++) {
+		//mas[i] = rand() % 101 - 50;
+		//cout << mas[i] << ' ';
+	//}
+	//for (int i = 0; i < n; i++) {
+		//if (mas[i] < 0)k++;
+
+	//}
+	//cout << k;
+
+	//САМ 3
+	//const int n = 10;
+	//int pr = 0;
+    //int mas[n];
+	//srand(time(nullptr));
+	//for (int i = 0; i < n; i++) {
+		//mas[i] = rand() % 41 - 20;
+		//cout << mas[i] << ' ';
+	//}
+	//for (int i = 0; i < n; i++) {
+		//pr = pr + 1;
+		
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
